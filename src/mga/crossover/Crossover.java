affbd610bdaae032a61f0f4676dcc26b04268592
Sander Verdonschot <sander.verdonschot+github@gmail.com>
2018-01-16 22:05:32 -0500

    Small JavaDoc changes.

--- src/mga/crossover/Crossover.java
+++ src/mga/crossover/Crossover.java
@@ -6,9 +6,10 @@ public interface Crossover<T> {
 
     /**
      * Forms two new children by crossing over parent1 and parent2.
+     *
      * @param parent1
      * @param parent2
-     * @return 
+     * @return
      */
     public abstract Pair<T, T> crossover(T parent1, T parent2);
 
--- src/mga/mutation/Mutation.java
+++ src/mga/mutation/Mutation.java
@@ -1,5 +1,12 @@
 package mga.mutation;
 
 public interface Mutation<T> {
+
+    /**
+     * Returns a new individual that is slightly changed from the given one.
+     *
+     * @param individual
+     * @return
+     */
     public abstract T mutate(T individual);
 }
